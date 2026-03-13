ed1ffa32272439e9016cdf45cfdac97769b9e3a9
Richard <richardadebowale.oye@gmail.com>
2026-03-13 15:48:00 -0400

    added user model

---
+++ app/models/user.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String, Boolean
+from .base import Base, TimeStamp
+import uuid
+
+
+class User(Base, TimeStamp):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+            UUID(as_uuid=True), 
+            primary_key=True, 
+            default=uuid.uuid4,
+            unique=True, 
+            nullable=False
+    )
+
+    email: Mapped[str] = mapped_column(String(300), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+
+    def __repr__(self) -> str:
+        return f"<User id: {self.id}, email: {self.email}>"
