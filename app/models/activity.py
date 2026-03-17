3859a560d676ca6c8a9473f39f625fc348d3cb5b
Richard <richardadebowale.oye@gmail.com>
2026-03-17 17:03:32 -0400

    full activity class

---
+++ app/models/activity.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String, ForeignKey
+from .base import Base, TimeStamp
+import uuid
+import enum
+
+
+class ActivityType(str, enum.Enum):
+    CREATED = "created"
+    UPDATED= "updated"
+    DELETED = "deleted"
+
+
+class ModelType(str, enum.Enum):
+    ORGANIZATIONS = "organizations" 
+    ORGANIZATION_MEMBERS = "organization_members"
+    USERS = "users"
+    TEAMS = "teams"
+    PROJECTS = "projects"
+    TASKS = "tasks"
+    MILESTONES = "milestones"
+
+
+class Activity(Base, TimeStamp):
+    __tablename__ = "activities"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+            UUID(as_uuid=True),
+            primary_key=True,
+            default=uuid.uuid4,
+            unique=True,
+            nullable=False
+    )
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"))
+    action: Mapped[ActivityType] = mapped_column(String, default="created", nullable=False)
+    model_type: Mapped[ModelType] = mapped_column(String, default="organization", nullable=False)
+    model_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), nullable=False)
+    org_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("organizations.id"))
+
