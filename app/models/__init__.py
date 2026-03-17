f447f6691f623d32d157a792a052adf8c6681273
Richard <richardadebowale.oye@gmail.com>
2026-03-17 17:12:08 -0400

    updating module import for Alembic Migrations of table

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from .org import Organization, OrganizationMember
+from .label import Label, TaskLabel
+from .team import Team, TeamMember
+from .milestone import Milestone
+from .activity import Activity
+from .project import Project
+from .task import Task
+from .user import User
