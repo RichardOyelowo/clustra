ab0e5845b587734b32ad6b1e263008340a3ce08a
Richard <richardadebowale.oye@gmail.com>
2026-03-19 21:43:59 -0400

    removed unnecesary import and syntax errors

--- app/config.py
+++ app/config.py
@@ -1,12 +1,11 @@
-from os import wait
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url = str
+    database_url: str
 
-    class config:
-        config = ".env"
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
