24b08693c0f251a93b5715f7e7754de9a7ad2bae
Richard <richardadebowale.oye@gmail.com>
2026-03-20 00:07:42 -0400

    added env variables for Token Manger class to config

--- app/config.py
+++ app/config.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str
+    secret_key: str
+    acesss_token_expiry: int
+    refresh_token_exppiry: int
 
     class Config:
         env_file = ".env"
