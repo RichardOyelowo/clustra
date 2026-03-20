162f10aab1071d5c4970ebb10dbb6fcea2d0987f
Richard <richardadebowale.oye@gmail.com>
2026-03-19 20:01:21 -0400

    created label and tasklabel model schema

--- app/schemas/label.py
+++ app/schemas/label.py
@@ -1,2 +1,28 @@
 from .base import BaseResponse
-from pydantic import
+from pydantic import BaseModel
+import uuid
+
+
+class LabelCreate(BaseModel):
+    name: str
+    color: str
+    proj_id: uuid.UUID
+
+
+class LabelResponse(BaseResponse):
+    id: uuid.UUID
+    name: str
+    color: str
+    proj_id: uuid.UUID
+
+
+class TaskLabelCreate(BaseModel):
+    label_id: uuid.UUID
+    task_id: uuid.UUID
+
+
+class TaskLabelResponse(BaseResponse):
+    id: uuid.UUID
+    label_id: uuid.UUID
+    task_id: uuid.UUID
+    
