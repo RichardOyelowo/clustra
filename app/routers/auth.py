5f3e47049146888b46109f600ec9436aca717c9b
Richard <richardadebowale.oye@gmail.com>
2026-03-22 23:57:51 -0400

    creating skeleton for user route

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,14 +1,16 @@
+from typing import Annotated
 from fastapi import APIRouter
+from ..schemas import UserCreate, UserResponse
 
 
 auth_router = APIRouter()
 
 
 @auth_router.post("/signup")
-def create_account():
+async def create_account(data: UserCreate) -> UserResponse:
     pass
 
 
-@auth_router.post("/login")
-def login():
+@auth_router.post("/login/")
+async def login(username, password):
     pass
