57e4668fe8d9fc05cf99941b10f13903c1cda2e7
Richard <richardadebowale.oye@gmail.com>
2026-03-23 00:12:09 -0400

    created endpoint for user routes

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,16 +1,19 @@
 from typing import Annotated
-from fastapi import APIRouter
+from ..database import db_session
+from fastapi import APIRouter, Depends
 from ..schemas import UserCreate, UserResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 auth_router = APIRouter()
 
 
 @auth_router.post("/signup")
-async def create_account(data: UserCreate) -> UserResponse:
+async def create_account(data: UserCreate, db: AsyncSession = Depends(db_session)) -> UserResponse:
     pass
 
 
 @auth_router.post("/login/")
-async def login(username, password):
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: AsyncSession = Depends(db_session)):
     pass
