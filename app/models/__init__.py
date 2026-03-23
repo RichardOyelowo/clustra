7d44ffb29fb8298bf721d3434cbbf85645591381
Richard <richardadebowale.oye@gmail.com>
2026-03-23 16:28:00 -0400

    updated module level import for code

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,8 @@
 from .org import Organization, OrganizationMember, OrganizationMemberRole
 from .label import Label, TaskLabel
-from .team import Team, TeamMember
-from .milestone import Milestone
-from .activity import Activity
-from .project import Project
-from .task import Task
+from .team import Team, TeamMember, TeamRole
+from .milestone import Milestone, MilestoneStatus
+from .activity import Activity, ActivityType
+from .project import Project, ProjectStatus
+from .task import Task, TaskPriority, TaskStatus
 from .user import User
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,8 +1,8 @@
+from .org import OrganizationCreate, OrganizationResponse, OrganizationMemberCreate, OrganizationMemberResponse
 from .label import LabelCreate, LabelResponse, TaskLabelCreate, TaskLabelResponse
 from .team import TeamCreate, TeamResponse, TeamMemberCreate, TeamMemberResponse
-from .milestone import MilestoneCreate, MilestoneResponse, MilestoneStatus
-from .activity import ActivityCreate, ActivityResponse, ActivityType
-from .project import ProjectCreate, ProjectResponse, ProjectStatus
-from .org import OrganizationCreate, OrganizationResponse
+from .milestone import MilestoneCreate, MilestoneResponse
+from .activity import ActivityCreate, ActivityResponse
+from .project import ProjectCreate, ProjectResponse
 from .user import UserCreate, UserResponse
 from .task import TaskCreate, TaskResponse
