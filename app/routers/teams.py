03d78fd1207623be56f3d265a0c0ca5b21fe2de6
Richard <richardadebowale.oye@gmail.com>
2026-03-25 16:01:13 -0400

    updated the routes fubction to async

--- app/routers/teams.py
+++ app/routers/teams.py
@@ -10,35 +10,35 @@ team_router = APIRouter(prefix="/orgs/{org_id}/teams")
 
 
 @team_router.post("/")
-def create_team(org_id: uuid.UUID, data: TeamCreate, db: AsyncSession = Depends(db_session)) -> TeamResponse:
+async def create_team(org_id: uuid.UUID, data: TeamCreate, db: AsyncSession = Depends(db_session)) -> TeamResponse:
     pass
 
 
 @team_router.get("/{team_id}")
-def get_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def get_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
 
 
 @team_router.patch("/{team_id}")
-def edit_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def edit_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
 
 
 @team_router.delete("/team_id}")
-def delete_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def delete_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
 
 
 @team_router.post("/teams/{team_id}/members")
-def add_team_member(org_id: uuid.UUID, data: TeamMemberCreate, db:AsyncSession = Depends(db_session)) -> TeamMemberResponse:
+async def add_team_member(org_id: uuid.UUID, data: TeamMemberCreate, db:AsyncSession = Depends(db_session)) -> TeamMemberResponse:
     pass
 
 
 @team_router.get("/teams/{team_id}/members")
-def get_team_members(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def get_team_members(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
 
 
 @team_router.delete("/teams/{team_id}/members/{id}")
-def delete_team_member(org_id: uuid.UUID, id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def delete_team_member(org_id: uuid.UUID, id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
