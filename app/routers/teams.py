e83c405f5c2c7669bcd0e67de46819e6ed686d60
Richard <richardadebowale.oye@gmail.com>
2026-03-25 16:57:52 -0400

    updated routes & parameters

--- app/routers/teams.py
+++ app/routers/teams.py
@@ -1,3 +1,4 @@
+from sqlalchemy.engine.interfaces import DBAPIModule
 from ..schemas import TeamMemberCreate, TeamMemberResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..schemas import TeamCreate, TeamResponse
@@ -14,31 +15,36 @@ async def create_team(org_id: uuid.UUID, data: TeamCreate, db: AsyncSession = De
     pass
 
 
+@team_router.get("/")
+async def get_teams(org_id: uuid.UUID,db: AsyncSession = Depends(db_session)):
+    pass
+
+
 @team_router.get("/{team_id}")
-async def get_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def get_team(org_id: uuid.UUID, team_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
 
 
 @team_router.patch("/{team_id}")
-async def edit_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def edit_team(org_id: uuid.UUID, team_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
 
 
 @team_router.delete("/team_id}")
-async def delete_team(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def delete_team(org_id: uuid.UUID, team_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
 
 
 @team_router.post("/{team_id}/members")
-async def add_team_member(org_id: uuid.UUID, data: TeamMemberCreate, db:AsyncSession = Depends(db_session)) -> TeamMemberResponse:
+async def add_team_member(org_id: uuid.UUID, team_id: uuid.UUID, data: TeamMemberCreate, db:AsyncSession = Depends(db_session)) -> TeamMemberResponse:
     pass
 
 
 @team_router.get("/{team_id}/members")
-async def get_team_members(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def get_team_members(org_id: uuid.UUID, team_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
 
 
 @team_router.delete("/{team_id}/members/{id}")
-async def delete_team_member(org_id: uuid.UUID, id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+async def delete_team_member(org_id: uuid.UUID, team_id: uuid.UUID, id: uuid.UUID, db: AsyncSession = Depends(db_session)):
     pass
