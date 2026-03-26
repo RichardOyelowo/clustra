ca7c9dcbd08f592ca7e6384930013c9bbee6b6e5
Richard <richardadebowale.oye@gmail.com>
2026-03-26 02:27:39 -0400

    created task and tasklabel model routes

---
+++ app/routers/labels.py
@@ -0,0 +1,62 @@
+from decimal import DecimalException
+from ..schemas import TaskLabelCreate, TaskLabelResponse
+from sqlalchemy.ext.asyncio import AsyncSession, async_object_session
+from ..schemas import LabelCreate, LabelResponse
+from fastapi import APIRouter, Depends
+from ..database import db_session
+import uuid
+
+label_router = APIRouter(prefix="/orgs/{org_id}/teams/{team_id}/")
+
+
+# ---------------- Project Label ------------------
+@label_router.get("/projects/{proj_id}")
+async def get_labels(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@label_router.post("/projects/{proj_id}")
+async def create_label(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, data: LabelCreate, db: AsyncSession = Depends(db_session)) -> LabelResponse:
+    pass
+
+
+@label_router.get("/projects/{proj_id}/{label_id}")
+async def get_label(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, label_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@label_router.patch("/projects/{proj_id}/{label_id}")
+async def edit_label(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, label_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@label_router.delete("/projects/{proj_id}/{label_id}")
+async def delete_label(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uui.UUID, label_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+# ------------------ Task Label ---------------------
+@label_router.get("/projects/{proj_id}/{label_id}/task_label")
+async def get_task_labels(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, label_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@label_router.post("/projects/{proj_id}/{label_id}/task_label/")
+async def create_task_label(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, label_id: uuid.UUID, data: TaskLabelCreate, db: AsyncSession = Depends(db_session)) -> TaskLabelResponse:
+    pass
+
+
+@label_router.get("/projects/{proj_id}/{label_id}/task_label/{task_id}")
+async def get_task_label(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, label_id: uuid.UUID, task_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@label_router.patch("/projects/{proj_id}/{label_id}/task_label/{task_id}")
+async def edit_task_label(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, label_id: uuid.UUID, task_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@label_router.delete("/projects/{proj_id}/{label_id}/task_label/{task_id}")
+async def delete_task_label(org_id: uuid.UUID, team_id: uuid.UUID, proj_id: uuid.UUID, label_id: uuid.UUID, task_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
