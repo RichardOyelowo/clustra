adc58c6350886779479cd6afd935e7b8d583fd45
Richard <richardadebowale.oye@gmail.com>
2026-03-26 03:42:29 -0400

    created routes for activity class

---
+++ app/routers/activity.py
@@ -0,0 +1,34 @@
+from ..schemas import ActivityCreate, ActivityResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, Depends
+from ..database import db_session
+import uuid
+
+
+activity_router = APIRouter(prefix="/orgs/{org_id}/activities")
+
+
+@activity_router.get("/")
+async def get_activities(org_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@activity_router.post("/")
+async def create_activity(org_id: uuid.UUID, data: ActivityCreate, db: AsyncSession = Depends(db_session)) -> ActivityResponse:
+    pass
+
+
+@activity_router.get("/{activity_id}")
+async def get_activity(org_id: uuid.UUID, activity_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@activity_router.patch("/{activity_id}")
+async def edit_activity(org_id: uuid.UUID, activity_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
+
+@activity_router.delete("/{activity_id}")
+async def delete_activity(org_id: uuid.UUID, activity_id: uuid.UUID, db: AsyncSession = Depends(db_session)):
+    pass
+
