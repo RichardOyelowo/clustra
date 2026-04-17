05017c6063a576caf1c83ecbb1216777f5fd1c9d
Richard <richardadebowale.oye@gmail.com>
2026-04-16 23:48:38 -0400

    updated service clas for 'Label' & 'TaskLabel'

--- app/services/label_service.py
+++ app/services/label_service.py
@@ -1,10 +1,11 @@
+from ..schemas import TaskLabelCreate, TaskLabelResponse, TaskLabelUpdate
 from ..schemas import LabelCreate, LabelResponse, LabelUpdate
-from ..schemas import TaskLabelCreate, TaskLabelResponse
 from ..utils.normalization import normalize_payloads
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import HTTPException
 from ..models import Label, TaskLabel
-from sqlalchemy import select, values
+from fastapi import HTTPException
+from sqlalchemy import select
+from typing import List
 from uuid import UUID
 
 
@@ -13,82 +14,160 @@ class LabelService:
     service class for label route related operations
     """
 
-    async def create_label(self, data: LabelCreate, db: AsyncSession):
-        result = await db.execute(select(Label).where(
-            Label.name == data.name,
-            Label.proj_id == data.proj_id
-        ))
-        existing = result.scalar_one_or_none()
+    # ---------------- Project Label ------------------
 
-        if existing:
-            raise HTTPException(status_code=409, detail="label already exists")
+    async def create_label(self, data: LabelCreate, db: AsyncSession) -> Label:
+        try:
+            result = await db.execute(select(Label).where(
+                Label.name == data.name,
+                Label.proj_id == data.proj_id
+            ))
+            existing = result.scalar_one_or_none()
 
-        label = Label(**normalize_payloads(data.model_dump()))
+            if existing:
+                raise HTTPException(status_code=409, detail="Label already exists")
 
-        db.add(label)
-        await db.commit()
-        await db.refresh(label)
+            label = Label(**normalize_payloads(data.model_dump()))
 
-        return label
+            db.add(label)
+            await db.commit()
+            await db.refresh(label)
 
-
-    async def get_labels(self, proj_id: UUID, db: AsyncSession):
-        result = await db.execute(select(Label).where(Label.proj_id == data.proj_id))
-        labels = result.scalars().all()
-
-        if not labels: return []
-
-        return labels
-
-
-    async def get_label(self, label_id: UUID, proj_id: UUID, db: AsyncSession):
-        result = await db.execute(select(Label).where(
-            Label.id == label_id,
-            Label.proj_id == proj_id
-        ))
-        label = result.scalar_one_or_none()
-
-        if not label: raise HTTPException(status_code=404, detail="Label doesn't exist")
-
-        return label
-
-
-    async def edit_label(self, label_id: UUID, data: LabelUpdate, db: AsyncSession):
-        result =  await db.execute(select(Label).where(
-            Label.id == label_id,
-            Label.proj_id == data.proj_id
-        ))
-        label = result.scalar_one_or_none()
-
-        if not label:
-            raise HTTPException(status_code=404, detail="Label doesn't exist")
-
-        data_dict = data.model_dump(exclude_unset=True)
-        if not data_dict:
             return label
+        except Exception as e:
+            await db.rollback()
+            raise e
 
-        updated_data = normalize_payloads(data_dict)
-        for field, value in updated_data.items():
-            setattr(label,field,value)
 
+    async def get_labels(self, proj_id: UUID, db: AsyncSession) -> List[Label]:
+        try:
+            result = await db.execute(select(Label).where(Label.proj_id == proj_id))
+            labels = result.scalars().all()
+
+            return labels
+        except Exception as e:
+            await db.rollback()
+            raise e
+
+
+    async def get_label(self, label_id: UUID, proj_id: UUID, db: AsyncSession) -> Label:
+        try:
+
+            result = await db.execute(select(Label).where(
+                Label.id == label_id,
+                Label.proj_id == proj_id
+            ))
+            label = result.scalar_one_or_none()
+
+            if not label: raise HTTPException(status_code=404, detail="Label doesn't exist")
+
+            return label
+        except Exception as e:
+            await db.rollback()
+            raise e
+
+
+    async def edit_label(self, label_id: UUID, data: LabelUpdate, db: AsyncSession) -> Label:
+        try:
+            result =  await db.execute(select(Label).where(
+                Label.id == label_id,
+                Label.proj_id == data.proj_id
+            ))
+            label = result.scalar_one_or_none()
+
+            if not label:
+                raise HTTPException(status_code=404, detail="Label doesn't exists")
+
+            data_dict = data.model_dump(exclude_unset=True)
+            if not data_dict:
+                return label
+
+            updated_data = normalize_payloads(data_dict)
+            for field, value in updated_data.items():
+                setattr(label,field,value)
+
+            await db.commit()
+            await db.refresh(label)
+            
+            return label
+        except Exception as e:
+            await db.rollback()
+            raise e
+
+
+    async def delete_label(self, label_id: UUID, proj_id: UUID, db: AsyncSession) -> dict:
+        try:
+            result = await db.execute(select(Label).where(
+                Label.id == label_id, 
+                Label.proj_id == proj_id
+            ))
+            label = result.scalar_one_or_none()
+
+            if not label:
+                raise HTTPException(status_code=404, detail="Label doesn't exists")
+
+            await db.delete(label)
+            await db.commit()
+            
+            return {"message": "label deleted successfully"}
+        except Exception as e:
+            await db.rollback()
+            raise e
+
+
+    # ------------------ Task Label ---------------------
+
+    async def create_task_label(self, data: TaskLabelCreate, db: AsyncSession) -> TaskLabel:
+        try:
+            result = await db.execute(select(TaskLabel).where(
+                TaskLabel.label_id == data.label_id,
+                TaskLabel.task_id == data.task_id
+            ))
+            existing = result.scalar_one_or_none()
+            if existing:
+                raise HTTPException(status_code=409, detail="Task Label already exists")
+
+            task_label = TaskLabel(**normalize_payloads(data.model_dump()))
+
+            db.add(task_label)
+            await db.commit()
+            await db.refresh(task_label)
+
+            return task_label
+        except Exception as e:
+            await db.rollback()
+            raise e
+
+
+    async def get_task_labels(self, task_id: UUID, db: AsyncSession) -> List[TaskLabel]:
+        result = await db.execute(select(TaskLabel).where(TaskLabel.task_id == task_id))
+        task_labels = result.scalars().all()
+
+        return task_labels
+
+
+    async def get_task_label(self, task_label_id: UUID, db: AsyncSession) -> TaskLabel:
+        result = await db.execute(select(TaskLabel).where(TaskLabel.id == task_label_id))
+        task_label = result.scalar_one_or_none()
+
+        if not task_label:
+            raise HTTPException(status_code=404, detail="Task Label doesn't exist")
+
+        return task_label
+
+
+    async def delete_task_label(self, task_label_id: UUID, db: AsyncSession) -> dict:
+        result = await db.execute(select(TaskLabel).where(TaskLabel.id == task_label_id))
+        task_label = result.scalar_one_or_none()
+        if not task_label:
+            raise HTTPException(status_code=404, detail="Task Label doesn't exist")
+
+        await db.delete(task_label)
         await db.commit()
-        await db.refresh(label)
-        
-        return label
+        return {"message": "Task label deleted successfully"}
 
 
-    async def delete_label(self, label_id: UUID, proj_id: UUID, db: AsyncSession):
-        result = await db.execute(select(Label).where(
-            Label.id == label_id, 
-            Label.proj_id == proj_id
-        ))
-        label = result.scalar_one_or_none()
-
-        if not label:
-            raise HTTPException(status_code=404, detail="Label doesn't exists")
-
-        await db.delete(label)
-        await db.commit()
-        await db.refresh(label)
-
-        return {"message": "label deleted successfully"}
+    async def get_task_labels_by_label(self, label_id: UUID, db: AsyncSession) -> List[TaskLabel]:
+        result = await db.execute(select(TaskLabel).where(TaskLabel.label_id == label_id))
+        task_labels = result.scalars().all()
+        return task_labels
