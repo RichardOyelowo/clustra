b85a51d17a63eb09be45bc13be353c7466788d0f
Richard <richardadebowale.oye@gmail.com>
2026-04-24 01:04:51 -0400

    updated import route to export path

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,8 @@
 from typing import Annotated
 from ..database import db_session
+from ..services import AuthService
 from fastapi import APIRouter, Depends
 from ..schemas import UserCreate, UserResponse
-from ..services.auth_service import AuthService
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordRequestForm
 
@@ -10,15 +10,12 @@ auth_router = APIRouter()
 auth_service = AuthService()
 
 @auth_router.post("/signup")
-async def create_account(data: UserCreate, db: AsyncSession = Depends(db_session)) -> UserResponse:
+async def create_account(data: UserCreate, db: AsyncSession = Depends(db_session)):
     user = await auth_service.register(data, db)
-
     return user
-
 
 
 @auth_router.post("/login/")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: AsyncSession = Depends(db_session)):
     tokens = await auth_service.login(form_data.username, form_data.password, db)
-
     return tokens
