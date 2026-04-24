685f5e1e3743c9c5b328897f17a8f8c0cb14f57e
Richard <richardadebowale.oye@gmail.com>
2026-04-24 00:00:47 -0400

    auth route functions implemented

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,17 +2,23 @@ from typing import Annotated
 from ..database import db_session
 from fastapi import APIRouter, Depends
 from ..schemas import UserCreate, UserResponse
+from ..services.auth_service import AuthService
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordRequestForm
 
 auth_router = APIRouter()
-
+auth_service = AuthService()
 
 @auth_router.post("/signup")
 async def create_account(data: UserCreate, db: AsyncSession = Depends(db_session)) -> UserResponse:
-    pass
+    user = await auth_service.register(data, db)
+
+    return user
+
 
 
 @auth_router.post("/login/")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: AsyncSession = Depends(db_session)):
-    pass
+    tokens = await auth_service.login(form_data.username, form_data.password, db)
+
+    return tokens
