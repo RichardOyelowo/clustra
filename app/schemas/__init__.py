6a35a3a2380cf3c1df698b9b7a7d3fab5e8de062
Richard <richardadebowale.oye@gmail.com>
2026-04-27 00:15:04 -0400

    added missing exports

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -6,21 +6,22 @@ This module provides convenient imports for all schema classes.
 
 from .org import OrganizationCreate, OrganizationMemberCreate, OrganizationMemberResponse, OrganizationResponse, OrganizationUpdate
 from .label import LabelCreate, LabelResponse, LabelUpdate, TaskLabelCreate, TaskLabelResponse, TaskLabelUpdate
-from .team import TeamCreate, TeamMemberCreate, TeamMemberResponse, TeamResponse
+from .team import TeamCreate, TeamResponse, TeamUpdate, TeamMemberCreate, TeamMemberResponse, TeamResponse
 from .milestone import MilestoneCreate, MilestoneResponse, MilestoneUpdate
+from .project import ProjectCreate, ProjectResponse, ProjectUpdate
 from .activity import ActivityCreate, ActivityResponse
 from .user import UserCreate, UserResponse, UserUpdate
-from .project import ProjectCreate, ProjectResponse
-from .task import TaskCreate, TaskResponse
+from .task import TaskCreate, TaskResponse, TaskUpdate
 
 
 __all__ = [
     "OrganizationMemberResponse", "OrganizationMemberCreate", "OrganizationResponse", "OrganizationCreate", "OrganizationUpdate",
-    "UserCreate", "UserUpdate", "UserResponse", "ProjectCreate", "ProjectResponse",
-    "TeamCreate", "TeamResponse", "TeamMemberCreate", "TeamMemberResponse",
+    "TeamCreate", "TeamResponse", "TeamUpdate", "TeamMemberCreate", "TeamMemberResponse",
     "TaskLabelCreate", "TaskLabelResponse", "TaskLabelUpdate",
     "MilestoneCreate", "MilestoneUpdate", "MilestoneResponse",
+    "ProjectCreate", "ProjectResponse", "ProjectUpdate",
     "LabelCreate", "LabelUpdate", "LabelResponse",
+    "UserCreate", "UserUpdate", "UserResponse",
+    "TaskCreate", "TaskResponse", "TaskUpdate",
     "ActivityCreate", "ActivityResponse",
-    "TaskCreate", "TaskResponse"
 ]
