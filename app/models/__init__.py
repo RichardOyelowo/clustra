9105668d3234040adf745ebc4fa4b52b38b106aa
Richard <richardadebowale.oye@gmail.com>
2026-05-16 23:00:34 -0400

    updated import statements for exported model class

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,23 +4,34 @@ Model module exports for the Clustra application.
 This module provides convenient imports for all model classes.
 """
 
-from .org import Organization, OrganizationMember, OrganizationMemberRole
+from .activity import Activity, ActivityType, ModelType
+from .label import Label, TaskLabel
 from .milestone import Milestone, MilestoneStatus
+from .org import Organization, OrganizationMember, OrganizationMemberRole
+from .project import Project, ProjectStatus
 from .task import Task, TaskPriority, TaskStatus
 from .team import Team, TeamMember, TeamRole
-from .activity import Activity, ActivityType
-from .project import Project, ProjectStatus
-from .label import Label, TaskLabel
 from .user import User
 
-
 __all__ = [
-    "Organization", "OrganizationMember", "OrganizationMemberRole",
-    "Task", "TaskPriority", "TaskStatus",
-    "Team", "TeamMember", "TeamRole",
-    "Milestone", "MilestoneStatus",
-    "Activity", "ActivityType",
-    "Project", "ProjectStatus",
-    "Label", "TaskLabel",
-    "User"
+    "Organization",
+    "OrganizationMember",
+    "OrganizationMemberRole",
+    "Task",
+    "TaskPriority",
+    "TaskStatus",
+    "Team",
+    "TeamMember",
+    "TeamRole",
+    "Milestone",
+    "MilestoneStatus",
+    "Activity",
+    "ActivityType",
+    "ModelType",
+    "Project",
+    "ProjectStatus",
+    "Label",
+    "TaskLabel",
+    "User",
 ]
+
