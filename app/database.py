1a142d88ee6910edf9f05612be795b27d4694671
Richard <richardadebowale.oye@gmail.com>
2026-05-28 11:34:06 -0400

    changed 'db-session' to re-raise errors

--- app/database.py
+++ app/database.py
@@ -8,9 +8,11 @@ async_session = async_sessionmaker(async_engine)
 
 async def db_session():
     async with async_session() as session:
-
         try:
             yield session
             await session.commit()
         except Exception:
             await session.rollback()
+            raise
+        finally:
+            await session.close()
