6854901d6be4d328d3a2779aa6c93c8d2835f213
Richard <richardadebowale.oye@gmail.com>
2026-05-30 18:59:41 -0400

    updated 'Settings' class to required & added test database

--- app/config.py
+++ app/config.py
@@ -1,11 +1,12 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import Field
 
 class Settings(BaseSettings):
-    database_url: str
-    secret_key: str
-    acesss_token_expiry: int
-    refresh_token_exppiry: int
+    database_url: str = Field(default=...)
+    test_database_url: str = Field(default=...)
+    secret_key: str = Field(default=...)
+    acesss_token_expiry: int = Field(default=...)
+    refresh_token_exppiry: int =Field(default=...)
 
     class Config:
         env_file = ".env"
