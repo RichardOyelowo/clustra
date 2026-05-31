ce56eb01488a84acabdc138a6798cb98eb82f9ad
Richard <richardadebowale.oye@gmail.com>
2026-05-31 08:34:51 -0400

    changed from 'Config' class to pydantic 'SettingsConfigDict'

--- app/config.py
+++ app/config.py
@@ -1,15 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     database_url: str = Field(default=...)
     test_database_url: str = Field(default=...)
     secret_key: str = Field(default=...)
     acesss_token_expiry: int = Field(default=...)
     refresh_token_exppiry: int =Field(default=...)
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
