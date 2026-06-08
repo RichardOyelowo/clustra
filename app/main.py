213cd4a7236ec25a6aee2b0109cb6b054ab98d1c
Richard <richardadebowale.oye@gmail.com>
2026-06-08 01:04:35 -0400

    mountd static files: html & css

--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
-from .routers import org_router
-from .routers import task_router, team_router
-from .routers import auth_router, user_router
-from .routers import proj_router, label_router
-from .routers import milestone_router, activity_router
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
+from .routers import (
+    activity_router,
+    auth_router,
+    label_router,
+    milestone_router,
+    org_router,
+    proj_router,
+    task_router,
+    team_router,
+    user_router,
+)
 
 app = FastAPI()
+
+
+# serve static & html Files
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+@app.get("/")
+async def root():
+    return FileResponse("frontend/login.html")
+
+@app.get("/{page}.html")
+async def serve_page(page: str):
+    return FileResponse(f"frontend/{page}.html")
 
 
 app.include_router(auth_router)
@@ -18,5 +38,3 @@ app.include_router(task_router)
 app.include_router(label_router)
 app.include_router(milestone_router)
 app.include_router(activity_router)
-
- 
