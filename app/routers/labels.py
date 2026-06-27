d31ab4e9b0b115f12fc622c5b91edb9069adf892
Richard <richardadebowale.oye@gmail.com>
2026-06-26 21:47:53 -0400

    Normalize label routes

--- app/routers/labels.py
+++ app/routers/labels.py
@@ -13,12 +13,12 @@ label_service = LabelService()
 
 
 # ---------------- Project Label ------------------
-@label_router.get("/", response_model=List[LabelResponse])
+@label_router.get("", response_model=List[LabelResponse])
 async def get_labels(org_id: UUID, team_id: UUID, proj_id: UUID, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
     return await label_service.get_labels(org_id, team_id, proj_id, current_user.id, db)
 
 
-@label_router.post("/", response_model=LabelResponse)
+@label_router.post("", response_model=LabelResponse)
 async def create_label(org_id: UUID, team_id: UUID, proj_id: UUID, data: LabelCreate, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
     return await label_service.create_label(org_id, team_id, proj_id, data, current_user.id, db)
 
@@ -40,21 +40,20 @@ async def delete_label(org_id: UUID, team_id: UUID, proj_id: UUID, label_id: UUI
 
 # ------------------ Task Label ---------------------
 @label_router.get("/{label_id}/task_label", response_model=List[TaskLabelResponse])
-async def get_task_labels(org_id: UUID, team_id: UUID, label_id: UUID, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
-    return await label_service.get_task_labels(org_id, team_id, label_id, current_user.id, db)
+async def get_task_labels(org_id: UUID, team_id: UUID, proj_id: UUID, label_id: UUID, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
+    return await label_service.get_task_labels(org_id, team_id, proj_id, label_id, current_user.id, db)
 
 
-@label_router.post("/{label_id}/task_label/", response_model=TaskLabelResponse)
-async def create_task_label(org_id: UUID, team_id: UUID, label_id: UUID, data: TaskLabelCreate, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
-    return await label_service.create_task_label(org_id, team_id, label_id, data, current_user.id, db)
+@label_router.post("/{label_id}/task_label", response_model=TaskLabelResponse)
+async def create_task_label(org_id: UUID, team_id: UUID, proj_id: UUID, label_id: UUID, data: TaskLabelCreate, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
+    return await label_service.create_task_label(org_id, team_id, proj_id, label_id, data, current_user.id, db)
 
 
 @label_router.get("/{label_id}/task_label/{task_label_id}", response_model=TaskLabelResponse)
-async def get_task_label(org_id: UUID, team_id: UUID, label_id: UUID, task_label_id: UUID, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
-    return await label_service.get_task_label(org_id, team_id, label_id, task_label_id, current_user.id, db)
+async def get_task_label(org_id: UUID, team_id: UUID, proj_id: UUID, label_id: UUID, task_label_id: UUID, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
+    return await label_service.get_task_label(org_id, team_id, proj_id, label_id, task_label_id, current_user.id, db)
 
 
 @label_router.delete("/{label_id}/task_label/{task_label_id}")
-async def delete_task_label(org_id: UUID, team_id: UUID, label_id: UUID, task_label_id: UUID, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
-    return await label_service.delete_task_label(org_id, team_id, label_id, task_label_id, current_user.id, db)
-
+async def delete_task_label(org_id: UUID, team_id: UUID, proj_id: UUID, label_id: UUID, task_label_id: UUID, current_user = Depends(validate_user), db: AsyncSession = Depends(db_session)):
+    return await label_service.delete_task_label(org_id, team_id, proj_id, label_id, task_label_id, current_user.id, db)
