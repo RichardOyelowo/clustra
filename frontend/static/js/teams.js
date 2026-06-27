67f2c986a45bcb6274958228470b8eae2e566e70
Richard <richardadebowale.oye@gmail.com>
2026-06-27 01:29:57 -0400

    Start teams page script

--- frontend/static/js/teams.js
+++ frontend/static/js/teams.js
@@ -1,3 +1,15 @@
 import { requireAuth, logout } from "./auth.js"
 import { renderSidebar } from ". /sidebar.js"
 import API from "./api.js"
+
+
+requireAuth()
+
+const params = new URLSearchParams(window.location.search)
+const orgId = params.get("org_id")
+
+function init() {
+
+}
+
+init()
