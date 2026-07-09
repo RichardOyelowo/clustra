4a2abf77265fd0aa693761559547f2aa24191365
Richard <richardadebowale.oye@gmail.com>
2026-07-09 18:34:43 -0400

    awaited response correctly

--- frontend/static/js/services.js
+++ frontend/static/js/services.js
@@ -1,32 +1,31 @@
-import API from "./api.js"
-
+import API from "./api.js";
 
 export async function getOrg(orgId) {
-    const response = await API.get(`/orgs/${orgId}`)
-    return response.ok ? await response.json() : null
+    const response = await API.get(`/orgs/${orgId}`);
+    return response.ok ? await response.json() : null;
 }
 
 export async function getTeams(orgId) {
-    const res = await API.get(`/orgs/${orgId}/teams`)
-    return res.ok ? await res.json() : []
+    const res = await API.get(`/orgs/${orgId}/teams`);
+    return res.ok ? await res.json() : [];
 }
 
 export async function getProjects(orgId, teamId) {
-    const res = await API.get(`/orgs/${orgId}/teams/${teamId}/projects`)
-    return res.ok ? await res.json() : []
+    const res = await API.get(`/orgs/${orgId}/teams/${teamId}/projects`);
+    return res.ok ? await res.json() : [];
 }
 
 export async function getOrgMembers(orgId) {
-    const res = await API.get(`/orgs/${orgId}/members`)
-    return res.ok ? res.json() : []
+    const res = await API.get(`/orgs/${orgId}/members`);
+    return res.ok ? await res.json() : [];
 }
 
 export async function getTeamMembers(orgId, teamId) {
-    const res = await API.get(`/orgs/${orgId}/teams/${teamId}/members`)
-    return res.ok ? res.json() : []
+    const res = await API.get(`/orgs/${orgId}/teams/${teamId}/members`);
+    return res.ok ? await res.json() : [];
 }
 
-export async function getUser(userId) {
-    const res = await API.get(`/user/me`)
-    return res.ok ? res.json() : console.error("failed to load user info")
+export async function getUser() {
+    const res = await API.get(`/user/me`);
+    return res.ok ? await res.json() : console.error("failed to load user info");
 }
