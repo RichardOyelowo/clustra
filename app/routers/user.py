01925ade5d356f8f3f14ac782d6276334efda0cc
Richard <richardadebowale.oye@gmail.com>
2026-07-17 17:21:04 -0400

    updated param type to 'UUID'

--- app/routers/user.py
+++ app/routers/user.py
@@ -4,7 +4,7 @@ from ..schemas import UserResponse, UserUpdate
 from ..dependencies import validate_user
 from ..services import UserService
 from ..database import db_session
-import uuid
+from uuid import UUID
 
 user_router = APIRouter(prefix="/user")
 user_service = UserService()
@@ -17,11 +17,11 @@ async def get_user(current_user = Depends(validate_user)):
 
 @user_router.get("/{id}", response_model=UserResponse)
 async def get_user_info(id: UUID, db: AsyncSession = Depends(db_session), current_user=Depends(validate_user)):
-    return user_service.get_user(id, db)
+    return await user_service.get_user(id, db)
 
 
 @user_router.patch("/{id}", response_model=UserResponse)
-async def update_user(id: uuid.UUID, data: UserUpdate, db: AsyncSession = Depends(db_session), current_user=Depends(validate_user)):
+async def update_user(id: UUID, data: UserUpdate, db: AsyncSession = Depends(db_session), current_user=Depends(validate_user)):
     if id != current_user.id:
         raise HTTPException(status_code=403, detail="Not allowed")
 
@@ -29,7 +29,7 @@ async def update_user(id: uuid.UUID, data: UserUpdate, db: AsyncSession = Depend
 
 
 @user_router.delete("/{id}")
-async def delete_user(id: uuid.UUID, db: AsyncSession = Depends(db_session), current_user=Depends(validate_user)):
+async def delete_user(id: UUID, db: AsyncSession = Depends(db_session), current_user=Depends(validate_user)):
     if id != current_user.id:
         raise HTTPException(status_code=403, detail="Not allowed")
 
